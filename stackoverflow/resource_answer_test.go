d21acae288bc4dceeb8b6e41947070acb6c9a770
SignalRichard <20459042+SignalRichard@users.noreply.github.com>
2025-08-28 11:03:19 -0400

    test: add acceptance tests

--- stackoverflow/resource_answer_test.go
+++ stackoverflow/resource_answer_test.go
@@ -10,56 +10,82 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-func TestStackOverflowAnswer(t *testing.T) {
+func TestAccStackOverflowAnswer(t *testing.T) {
+	resourceType := "stackoverflow_answer"
+	resourceName := "test"
+	resourceIdentifier := fmt.Sprintf("%s.%s", resourceType, resourceName)
+
 	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccStackOverflowAnswerCheckDestroy(resourceIdentifier),
 		Steps: []resource.TestStep{
 			{
-				Config: testStackOverflowAnswerConfig(),
+				Config: testAccStackOverflowAnswerConfig(resourceType, resourceName, ""),
 				Check: resource.ComposeTestCheckFunc(
-					testStackOverflowAnswerExists("stackoverflow_answer.test"),
+					testAccStackOverflowAnswerCheckExists(resourceIdentifier),
+				),
+			},
+			{
+				Config: testAccStackOverflowAnswerConfig(resourceType, resourceName, " Updated"),
+				Check: resource.ComposeTestCheckFunc(
+					testAccStackOverflowAnswerCheckExists(resourceIdentifier),
 				),
 			},
 		},
 	})
 }
 
-func testStackOverflowAnswerConfig() string {
-	return `resource "stackoverflow_answer" "test" {
-		question_id = 58
-		body_markdown = "Unit test."
-	}`
-}
-
-func testStackOverflowAnswerDestroy(s *terraform.State) error {
-	c := testAccProvider.Meta().(*so.Client)
-
-	for _, resource := range s.RootModule().Resources {
-		if resource.Type != "stackoverflow_answer" {
-			continue
-		}
-
-		answerID, err := strconv.Atoi(resource.Primary.ID)
-		if err != nil {
-			return err
-		}
-
-		questionID, err := strconv.Atoi(resource.Primary.Attributes["question_id"])
-		if err != nil {
-			return err
-		}
-
-		err = c.DeleteAnswer(questionID, answerID)
-		if err != nil {
-			return err
-		}
+func testAccStackOverflowAnswerConfig(resourceType, resourceName, update string) string {
+	return fmt.Sprintf(`resource "stackoverflow_question" "test" {
+		title = "How do I run the Terraform Provider Acceptance Tests?"
+		body_markdown = "I want to run the Terraform Provider for Stack Overflow acceptance tests, how do I do that?"
+		tags = ["test"]
 	}
-
-	return nil
+	resource "%s" "%s" {
+		question_id = stackoverflow_question.test.id
+		body_markdown = "Unit test%s."
+	}`, resourceType, resourceName, update)
 }
 
-func testStackOverflowAnswerExists(n string) resource.TestCheckFunc {
+func testAccStackOverflowAnswerCheckDestroy(resourceIdentifier string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		resourceState, found := s.RootModule().Resources[resourceIdentifier]
+
+		if !found {
+			return fmt.Errorf("No resource found with id %s", resourceIdentifier)
+		}
+
+		if resourceState.Primary.ID == "" {
+			return fmt.Errorf("Resource with ID is empty for %s", resourceIdentifier)
+		}
+
+		answerID, err := strconv.Atoi(resourceState.Primary.ID)
+		if err != nil {
+			return err
+		}
+
+		questionID, err := strconv.Atoi(resourceState.Primary.Attributes["question_id"])
+		if err != nil {
+			return err
+		}
+
+		c := testAccProvider.Meta().(*so.Client)
+		answer, err := c.GetAnswer(&questionID, &answerID)
+
+		if err != nil {
+			return err
+		}
+
+		if answer != nil && !answer.IsDeleted {
+			return fmt.Errorf("Resource still exists for %s", resourceIdentifier)
+		}
+
+		return nil
+	}
+}
+
+func testAccStackOverflowAnswerCheckExists(n string) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		resource, ok := s.RootModule().Resources[n]
 
